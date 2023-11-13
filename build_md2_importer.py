ea264e7086e176eaea4ea2f73b9992ce281aa550
Lennart Greifenhain <lennart.greifenhain@pixelbiotech.com>
2023-11-13 17:42:49 +0100

    add plugin building script

---
+++ build_md2_importer.py
@@ -0,0 +1,22 @@
+import os.path
+import shutil
+
+# files to include in the output zip file
+files = [
+    "MD2.py",
+    "io_import_md2/__init__.py",
+    "io_import_md2/blender_load_md2.py"
+]
+
+# intermediary location for the directory to be zipped
+dest = "build/io_import_md2"
+
+if os.path.exists("build") and os.path.isdir("build"):
+    shutil.rmtree("build")
+os.makedirs(dest)
+
+for file in files:
+    shutil.copyfile(file, os.path.join(dest, os.path.basename(file)))
+
+# create zip file
+shutil.make_archive("blender-md2-importer", 'zip', "build")
