042d7d14a5fac98bc81720537b705b4c2d289810
Lennart Greifenhain <lennart.greifenhain@pixelbiotech.com>
2023-11-13 17:43:35 +0100

    add pytest unit tests checking no exception raised for md2 files and ValueError raised for jpg file

---
+++ tests/test_md2_noerror.py
@@ -0,0 +1,19 @@
+import os
+import MD2
+import pytest
+
+
+def test_models():
+    data_dir = "tests/data"
+    models = os.listdir(data_dir)
+    models = [x for x in models if x.lower().endswith(".md2")]
+
+    for model in models:
+        MD2.load_file(os.path.join(data_dir, model))
+    assert True
+
+
+def test_wrong_format():
+    with pytest.raises(ValueError):
+        MD2.load_file('tests/data/car.jpg')
+
